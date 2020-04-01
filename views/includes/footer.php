a970dc4e0f29698705cadd3aa560a527d89a1952
yu-design <info@yu-design.be>
2020-04-02 00:06:57 +0200

    Sauvegarde de sécurité

---
+++ views/includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted">E-Shop</span>
+    </div>
+</footer>
---
+++ views/includes/menu_admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="<?ROOT_PATH?>public/css/boostrap.min.css">
+        <link rel="stylesheet" href="<?ROOT_PATH?>public/css/sticky-footer-navbar.css">
+        <script src="<?=ROOT_PATH?>public/js/jquery-3.4.1.slim.min.js"></script>
+        <script src="<?=ROOT_PATH?>public/js/popper.min.js"></script>
+        <script src="<?=ROOT_PATH?>public/js/bootstrap.min.js"></script>
+        <title><?php echo $title; ?></title>
+    </head>
+    <body>
+        <nav>
+            
+        </nav>
+    </body>
+</html>
