f3c91106ac9f8574505edb1c77f488ba40cc5a24
yu-design <info@yu-design.be>
2020-04-01 17:52:25 +0200

    Ajout du template (vide) + img caddie podifiée

---
+++ View/includes/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?php echo $titre; ?></title>
+    </head>
+    <body>
+        <h2><?php echo $titre; ?></h2>
+        <?php echo $contenu; ?>
+
+    </body>
+</html>
