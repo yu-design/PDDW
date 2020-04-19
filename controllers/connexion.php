02df5bb981894e7fae3115a69a2698e4009c38ce
yu-design <info@yu-design.be>
2020-04-20 01:28:13 +0200

    User + login + connexion (pas encore fonctionnel :( )

---
+++ controllers/connexion.php
@@ -0,0 +1,48 @@
+<?php
+    include 'views/includes/head.php';
+    include 'views/includes/nav.php';
+    require 'models/users.php';
+    $titre="Connexion";
+    function isValidUser($login, $password) {
+        $user = getUserByLogin($login);
+        //Ici on va vérifier si le login/pass est bon
+        if($user && password_verify($password, $user['Pass'] ))
+        {
+            return $user;
+        }
+    }
+
+    if(!empty($_SESSION['id'])){
+        header("Location: ".ROOT_PATH);
+        exit();
+    }
+
+    if(!empty($_POST)) {
+        if(!empty($_POST['login']) && !empty($_POST['password']))
+        {
+            $user = isValidUser($_POST['login'], $_POST['password']);
+            if($user)
+            {
+                //Authentification OK
+                $_SESSION['id'] = $user['ID'];
+                $_SESSION['message'] = "Bienvenue ".$user['Pseudo'];
+                header("Location: ".ROOT_PATH);
+                exit();
+            }
+            else
+            {
+                //Authentification NOK
+                $_SESSION['error'] = "Mauvais login et/ou password !";
+            }
+        }
+        else
+        {
+            //Ici on va prévenir l'utilisateur qu'il manque quelque chose
+            $_SESSION['error'] = "Il manque une information login ou password !";
+        }
+    }
+
+    include 'views/connexion.php';
+    include 'views/includes/main.php';
+    include 'views/includes/footer.php';
+?>
--- models/users.php
+++ models/users.php
@@ -2,7 +2,7 @@
 require_once 'models/db.php';
 
 function getUserById($id) {
-    $reponse = $db->prepare('SELECT * FROM utilisateur WHERE id = :id');
+    $reponse = $db->prepare('SELECT * FROM utilisateur WHERE ID = :id');
     $reponse->execute([':id' => $id]);
     $user = $reponse->fetch();
     $reponse->closeCursor(); // Termine le traitement de la requête
@@ -10,7 +10,7 @@ function getUserById($id) {
 }
 
 function getUserByLogin($login) {
-    $reponse = $db->prepare('SELECT * FROM utilisateur WHERE login = :login');
+    $reponse = $db->prepare('SELECT * FROM utilisateur WHERE Pseudo = :login');
     $reponse->execute([':login' => $login]);
     $user = $reponse->fetch();
     $reponse->closeCursor(); // Termine le traitement de la requête
@@ -20,12 +20,12 @@ function getUserByLogin($login) {
 function setUser($id, $login, $email, $password, $role) {
     $user = getUserById($id);
     //C'est ici qu'on va faire l'update de l'utilisateur.
-    $reponse = $db->prepare('UPDATE utilisateur SET login = :login, email = :email, password = :password, role = :role WHERE id = :id');
+    $reponse = $db->prepare('UPDATE utilisateur SET Pseudo = :login, Mail = :email, Pass = :password, Role = :role WHERE ID = :id');
     if($password){
         $password = password_hash($password, PASSWORD_DEFAULT);
     }
     else {
-        $password = $user['password'];
+        $password = $user['Pass'];
     }
     $reponse->execute([':id' => $id, ':email' => $email, ':password' => $password, ':login' => $login]);
     $reponse->closeCursor(); // Termine le traitement de la requête
