4bdfa9ab58a8e50a0dcaae5eb5e3739f5959095e
yu-design <info@yu-design.be>
2020-09-02 21:11:41 +0200

    code pour statistique

--- controllers/statistiques.php
+++ controllers/statistiques.php
@@ -2,6 +2,17 @@
     $titre = "Statistiques des ventes";
     include 'views/includes/head.php';
     require 'navControler.php';
+    require 'models/article.php';
+
+    global $db;
+    $reponse = "SELECT COUNT(VA.Article_ID) AS 'Nombre', a.Titre AS 'Titre', A.ID 
+                FROM article AS a 
+                INNER JOIN venteArticle AS VA ON VA.Article_ID = a.ID 
+                INNER JOIN vente AS V ON V.ID = VA.Vente_ID
+                GROUP BY A.ID ORDER BY COUNT(VA.Article_ID) DESC LIMIT 5;";
+    $stmt = $db->prepare($reponse);
+    $stmt->execute();
+    $arr = $stmt->fetchAll();
 
     navControl();
     include 'views/statistique.php';
--- views/statistique.php
+++ views/statistique.php
@@ -1,6 +1,27 @@
 <?php ob_start() ?>
+    <script type='text/javascript'>
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawCharts);
 
-    <script type="text/javascript" src="public\js\charts.js"></script>
+        function drawCharts(){
+        // Create the data table.
+            var data = new google.visualization.arrayToDataTable([
+                ['Titre','nombre'],
+                <?php foreach($arr as $key=>$val){?>
+                    ['<?= $val['Nombre']?>','<?= $val['Titre']?>'],
+                <?php}?>
+            ]);
+
+            var options = {
+                title: 'Les 5 meilleures ventes',
+                width: 800,
+                heigh: 350,
+            };
+            
+            var chart = new google.visualization.PieChart(document.getElementById('charts'));
+            chart.draw(data, options);
+        }
+    </script>
 
     <div id="charts"></div>
 <?php
